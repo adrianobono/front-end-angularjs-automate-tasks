908609edcd5d38fee0f33d5ab91eb30fe2d9c789
bonoworks <bonoworks@gmail.com>
2019-03-19 00:00:09 -0300

    up front

--- web/index.js
+++ web/index.js
@@ -39,6 +39,9 @@ function setList() {
 
 
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
   
     next();
 
